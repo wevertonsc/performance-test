da77e7de114996ca22e49e30866bf8932fc1bc0a
Weverton de Souza Castanho <weverton.castanho@AIR-DE-WEVERTON.station>
2022-02-11 22:37:02 +0000

    Initial commit

---
+++ src/main/java/ie/tudublin/performancetest/PerformanceTestApplication.java
@@ -0,0 +1,13 @@
+package ie.tudublin.performancetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceTestApplication.class, args);
+	}
+
+}
